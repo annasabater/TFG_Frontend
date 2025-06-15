07ed5f9a5030f3e789bdb3ddbc4fb355f62eae72
Joaquin Roca <joaquinrocarod@gmail.com>
2025-06-16 01:42:32 +0200

    Google Login: Web

--- lib/api/google_signin_api.dart
+++ lib/api/google_signin_api.dart
@@ -1,7 +1,8 @@
 import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleSignInApi {
-  static final _googleSignIn = GoogleSignIn();
+  static final _clientIDWeb = '438990465601-419ub663pi6il99gpeakm3osrog1s1ea.apps.googleusercontent.com';
+  static final _googleSignIn = GoogleSignIn(clientId: _clientIDWeb);
   static Future<GoogleSignInAccount?> login() => _googleSignIn.signIn();
 
   static Future logout() => _googleSignIn.disconnect();
